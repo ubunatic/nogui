69c54116b4a0add6f4235bbd75d07d30f2d958f5
Uwe Jugel <uwe.jugel@lovoo.com>
2021-05-16 16:36:13 +0200

    more useful assert funcs as needed in the tests

--- src/assert.js
+++ src/assert.js
@@ -26,6 +26,24 @@ function False(val, msg='assert.False', ...o) {
     assert(!val, msg, ...o)
 }
 
+function InstanceOf(val, T, msg='assert.InstanceOf', ...o) {
+    assert(val instanceof T, `${msg}, got ${typ(val)}, expected ${T}`, ...o)
+}
+
+function Match(val, expr, capt={}, msg='assert.Match', ...o) {
+    let m = `${val}`.match(expr)
+    assert(m != null, `${msg}, value ${val} does not match expr ${expr}`, ...o)
+    let errors = []
+    if (capt) for (const k in capt) {
+        let cap = capt[k]
+        let got = m[k]
+        if (got != cap) errors.push(`capture group ${k} expected "${cap}", got "${got}"`)
+    }
+    if (errors.length > 0) {
+        assert(false, `${msg} capture errors: ${errors.join(', ')}`, ...o)
+    }
+}
+
 function Fail(msg, ...o) {
     assert(false, msg, ...o)
 }
@@ -36,12 +54,14 @@ function assert(val, msg='assert', ...o) {
 
 if (!this.module) this.module = {}
 module.exports = {
-    Null:    Null,
-    NotNull: NotNull,
-    Eq:      Eq,
-    NotEq:   NotEq,
-    True:    True,
-    False:   False,
-    Fail:    Fail,
-    assert:  assert,
+    assert,
+    Null,
+    NotNull,
+    Eq,
+    NotEq,
+    True,
+    False,
+    Fail,
+    InstanceOf,
+    Match,
 }
