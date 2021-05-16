2acf7148885bccadb2c2ca36e77f357a98064e33
Uwe Jugel <uwe.jugel@lovoo.com>
2021-05-16 16:32:45 +0200

    adding makePath to make paths easily

--- src/sys.js
+++ src/sys.js
@@ -5,7 +5,7 @@
 const { GLib, } = imports.gi
 const ByteArray = imports.byteArray
 
-/** converts path elements `...s` to an OS-specific path string
+/** converts path elements to an OS-specific path string
  * @returns {string} OS-specific path string
 */
 var toPath = (...s) => GLib.build_filenamev(s)
@@ -22,7 +22,7 @@ var toPathArray = (path) => (typeof path == 'string')? [path] : path
  * @param {string|Array} file  file path as string or Array
  * @returns {string}           extension of the file
 */
-var fileExt = (file) => GLib.build_filenamev(toPathArray(file)).split('.').pop()
+var fileExt = (file) => basename(toPathArray(file)).split('.').pop()
 
 /**
  * @param {string} path  the file name of the file to read
@@ -30,5 +30,23 @@ var fileExt = (file) => GLib.build_filenamev(toPathArray(file)).split('.').pop()
  */
 var readFile = (path) => ByteArray.toString(GLib.file_get_contents(path)[1])
 
+/** converts string of path Array to a path string
+ * @param   {string|Array} path  file path as string or Array
+ * @returns {string}             file path as string
+*/
+var makePath = (path) => (typeof path == 'string')? path : toPath(...path)
+
+/**
+ * @param {string} path  the file path to get the dirname from
+ * @returns {string}     the dirname of the path
+*/
+var dirname = (path) => GLib.path_get_dirname(makePath(path))
+
+/**
+* @param {string} path  the file path to get the basename from
+* @returns {string}     the basename of the path
+*/
+var basename = (path) => GLib.path_get_basename(makePath(path))
+
 if (!this.module) this.module = {}
-module.exports = { toPath, toPathArray, fileExt, readFile }
+module.exports = { toPath, toPathArray, fileExt, readFile, makePath, dirname, basename }
